623b674f8cf90ab616c10117663764d0470e08e2
Aiko Beniyama <bennie.a.320@gmail.com>
2022-07-17 12:04:56 +0900

    エキスパンション略称の取得
    商品名からエキスパンション名を取得する処理を追加。

--- js/catecories.js
+++ js/catecories.js
@@ -42,12 +42,6 @@ fileReader.onload = () => {
     return result;
   });
 
-  // 言語
-  const langs = {"日本語":"[JP]","英語":"[EN]" };
-
-// Foil/非Foil
-  const isFoil = {"Yes":"【Foil】", "No": ""};
-
   //TSVの内容を表示
   let textarea = "";
   for (item of items){
@@ -58,50 +52,57 @@ fileReader.onload = () => {
     let id = item['商品ID'];
     name = name.replaceAll('\"', '');
     textarea += id + ',' + name + "\r\n";
+    let attrs = name.split(/【(.*?)】/).filter(function(value, index){
+      if (value == '' || value == 'Foil') {
+        return;
+      }
+      return value;
+    });
+    console.log(attrs);
   }
   $('textarea').text(textarea);
   message.innerHTML = items.length + "件のデータを読み込みました。";
 
-  // expansion.xmlの読み込み
-  // $.ajax({
-  //   url: 'xml/expansion.xml',
-  //   type: 'GET',
-  //   dataType: 'XML',
-  //   cache: false,
-  //   success:function(data) {
-  //     var expansions = [];
-  //     $(data).find('row').each(function() {
-  //       var url = $(this).find('ulink').attr('url');
-  //       var paras = $(this).find('para').map(function() {
-  //         return $(this).text();
-  //       });
-  //       expansions[url] = "【" + paras[1] + "】";
-  //     });
-  //     for (item of items) {
-  //       if (item.cardname != "") {
-  //         console.log();
-  //         var ex = expansions[item['エキスパンション']];
-  //         cardname =  isFoil[item.Foil] + ex +  item.cardname;
-  //         var lang = langs[item['言語']] ;
-  //         var en_name = item['英名'];
-  //         if (lang == '[EN]') {
-  //           cardname +=  "/" + en_name;
-  //         }
-  //         cardname += lang;
-  //         pic1 = en_name.toLowerCase().replace("\'", "").replace(" ", "_");
-  //         pic2 = pic1 + "_rev";
-  //         textarea += "," + cardname + ",,,," + item['価格'] + ",1" + "," + item['枚数'] + "," +
-  //                     item['公開'] +  "," + pic1 + ".jpg," + pic2 + ".jpg\r\n";
-  //       }
-  //     }
-  //     document.getElementById("cardnames").value = textarea;
-  //     // tbody.innerHTML = tbody_html;
-  //     message.innerHTML = items.length + "件のデータを読み込みました。";
-  //   },
-  //   error:function(err) {
-  //     console.log(err);
-  //   }
-  // });
+  //expansion.xmlの読み込み
+  $.ajax({
+    url: 'xml/expansion.xml',
+    type: 'GET',
+    dataType: 'XML',
+    cache: false,
+    success:function(data) {
+      var expansions = [];
+      $(data).find('row').each(function() {
+        var url = $(this).find('ulink').attr('url');
+        var paras = $(this).find('para').map(function() {
+          return $(this).text();
+        });
+        expansions[url] = "【" + paras[1] + "】";
+      });
+      // for (item of items) {
+      //   if (item.cardname != "") {
+      //     console.log();
+      //     var ex = expansions[item['エキスパンション']];
+      //     cardname =  isFoil[item.Foil] + ex +  item.cardname;
+      //     var lang = langs[item['言語']] ;
+      //     var en_name = item['英名'];
+      //     if (lang == '[EN]') {
+      //       cardname +=  "/" + en_name;
+      //     }
+      //     cardname += lang;
+      //     pic1 = en_name.toLowerCase().replace("\'", "").replace(" ", "_");
+      //     pic2 = pic1 + "_rev";
+      //     textarea += "," + cardname + ",,,," + item['価格'] + ",1" + "," + item['枚数'] + "," +
+      //                 item['公開'] +  "," + pic1 + ".jpg," + pic2 + ".jpg\r\n";
+      //   }
+      // }
+      // document.getElementById("cardnames").value = textarea;
+      // // tbody.innerHTML = tbody_html;
+      // message.innerHTML = items.length + "件のデータを読み込みました。";
+    },
+    error:function(err) {
+      console.log(err);
+    }
+  });
 }
 
 // ファイル読み取り失敗時
